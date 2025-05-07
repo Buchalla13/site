aa5acd48da12041731e136523383c9e1c6a5d4ca
Buchalla Monstro <jpbuchalla23@gmail.com>
2025-05-07 12:24:38 -0300

    Add files via upload

---
+++ 7_05_Buchala/java/java.js
@@ -0,0 +1,12 @@
+let cor = "branco";
+
+switch(cor){
+    case "preto":
+        console.log("macaco")
+        break;
+    case "branco":
+        console.log("bem vindo normal :D")
+        break;
+    default:
+        console.log("morra")
+}
